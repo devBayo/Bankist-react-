3f4e461fa4fd05d90bb346fb676e27b8ec947cc6
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-28 07:25:03 +0100

    added movement item

--- src/components/Main/Movements/MovementItem.jsx
+++ src/components/Main/Movements/MovementItem.jsx
@@ -1,9 +1,20 @@
-// import classes from './MovementItem.module.css';
+import classes from './MovementItem.module.css';
 
 const MovementItem = props => {
-  // deposit -- withdrawal
-
-  return <li></li>;
+  return (
+    <li className={classes['movements__row']}>
+      <div
+        className={`${classes['movements__type']} ${
+          classes[`movements__type--${props.type}`]
+        }`}
+      >
+        {props.index} {props.type}
+      </div>
+      <div className={classes['movements__date']}></div>
+      {/* <div className={classes['movements__date']}>24/01/2037</div> */}
+      <div className={classes['movements__value']}>{props.value}€</div>
+    </li>
+  );
 };
 
 export default MovementItem;
