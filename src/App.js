b302acd1b0b04e65229af4a8ece3a62ef36505b8
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-28 07:21:29 +0100

    consumed UsersContextProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import { AuthContextProvider } from './contexts/auth-context';
+import { UsersContextProvider } from './contexts/users-context';
 
 function App() {
   return (
-    <AuthContextProvider>
+    <UsersContextProvider>
       <Header />
       <Main />
-    </AuthContextProvider>
+    </UsersContextProvider>
   );
 }
 
