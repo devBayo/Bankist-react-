7f49cd7031d873b4082214c63e0a271d6b77acb5
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-10-28 04:17:04 +0100

    used authcontextprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import { AuthContextProvider } from './contexts/auth-context';
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Header />
       <Main />
-    </>
+    </AuthContextProvider>
   );
 }
 
